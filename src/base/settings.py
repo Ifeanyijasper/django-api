71083b71c771752b88bea1047acdf953d043db1c
ifeanyijasper <ifeanyijasper11@gmail.com>
2024-05-10 23:36:29 +0100

    Popular topic section part 1

--- src/base/settings.py
+++ src/base/settings.py
@@ -122,6 +122,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR.parent / "local-cdn" / "static"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
